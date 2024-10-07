4524d3fd9e400cdc8ecf3142ac0fa5c2f0a8c991
Maximilian Weber <e11928905@student.tuwien.ac.at>
2024-10-07 21:27:28 +0200

    initial commit, basic html structure

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <div class="header_text">DITHER</div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name: 'Header',
+    }
+</script>
+
+<style scoped>
+
+  @font-face {
+    font-family: 'CustomFont'; /* Name your font family */
+    src: url('/Users/mw/Library/Fonts/Blur Bold.otf'); /* Locate the .ttf file within your directory*/
+  }
+
+  body{
+    background-color: #f2f2f2;
+    margin: 0;
+  }
+  header {
+    border-right: 0.1px solid;
+    position: sticky;    
+  }
+  .header_text{
+    font-family:CustomFont;
+    text-align: left;
+    color: black;
+    padding: 20px;
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+  }
+</style>
